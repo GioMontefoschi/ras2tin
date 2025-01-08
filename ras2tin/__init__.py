688f404343c58206e0696f1aab5314612b41c58e
GioMontefoschi <giovannimontefoschi@gmail.com>
2025-01-08 18:07:14 +0100

    More organizing and __init__.py

---
+++ ras2tin/__init__.py
@@ -0,0 +1,16 @@
+# Import the main utility functions
+from .utils import pad_array, conv2d
+
+# Import the VIP selection algorithm
+from .vip_selection import vip_selection
+
+# Import the TIN class
+from .ras2tin import TIN
+
+# Define what will be imported when `from ras2tin import *` is used
+__all__ = [
+    "pad_array",
+    "conv2d",
+    "vip_selection",
+    "TIN",
+]

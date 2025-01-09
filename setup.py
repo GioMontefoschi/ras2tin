fb185bcf19d3421e5c099577a4719800d254c8ff
GioMontefoschi <giovannimontefoschi@gmail.com>
2025-01-09 12:11:12 +0100

    cleanup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ras2tin",  # Name of your package
+    version="0.1",  # Version number
+    packages=find_packages(where="ras2tin"),  # This will include the ras2tin directory and its submodules
+    package_dir={"": "ras2tin"},  # Specifies that the package is inside the ras2tin directory
+    install_requires=[
+        'numpy',
+        'rasterio',
+        'scipy',
+        'plotly'
+    ],
+)
